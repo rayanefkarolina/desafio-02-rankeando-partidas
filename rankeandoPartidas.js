c349e3ccb8609f41a1e090088c6d2c4c86133bac
rayanefkarolina <rayane.karolina@a.ucb.br>
2024-10-09 12:34:49 -0300

    desafio 02 concluído

---
+++ rankeandoPartidas.js
@@ -0,0 +1,38 @@
+let vitorias = 71;
+let derrotas = 1;
+
+function calcularNivel(vitorias, derrotas) {
+  let saldoVitorias = vitorias - derrotas;
+  let nivel;
+
+  if (vitorias <= 10) {
+    nivel = "Ferro";
+  } else if (vitorias >= 11 && vitorias <= 20) {
+    nivel = "Bronze";
+  } else if (vitorias >= 21 && vitorias <= 50) {
+    nivel = "Prata";
+  } else if (vitorias >= 51 && vitorias <= 80) {
+    nivel = "Ouro";
+  } else if (vitorias >= 81 && vitorias <= 90) {
+    nivel = "Diamante";
+  } else if (vitorias >= 91 && vitorias <= 100) {
+    nivel = "Lendário";
+  } else if (vitorias >= 101) {
+    nivel = "Imortal";
+  }
+
+  return {
+    saldoVitorias: saldoVitorias,
+    nivel: nivel,
+  };
+}
+
+function exibirResultado() {
+  let resultado = calcularNivel(vitorias, derrotas);
+
+  console.log(
+    `O Herói tem um saldo de ${resultado.saldoVitorias} e está no nível de ${resultado.nivel}`
+  );
+}
+
+exibirResultado();
